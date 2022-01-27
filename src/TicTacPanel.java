41ca37302f0e52ae0e0213bc6d9086e25d691671
Rishi <V0902136@students.katyisd.org>
2022-01-27 13:01:55 -0600

    test 8 Rishis computer

--- src/TicTacPanel.java
+++ src/TicTacPanel.java
@@ -16,75 +16,71 @@ public class TicTacPanel extends JPanel implements MouseListener {
     String name;
 
     public TicTacPanel() {
-            super();
-            setSize(800,600);
-            buffer = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_4BYTE_ABGR);
-            addMouseListener(this);
-            rand = new Random();
-        }
+        super();
+        setSize(800,600);
+        buffer = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_4BYTE_ABGR);
+        addMouseListener(this);
+        rand = new Random();
+    }
 
-        public void paint(Graphics g) {
-            Graphics bg = buffer.getGraphics();
-            bg.setColor(Color.BLACK);
-            bg.fillRect(0,0,getWidth(),getHeight());
-            bg.setColor(Color.WHITE);
-            for (int x = 0; x < locations.length; x++) {
-                for (int y = 0; y < locations.length; y++) {
-                    for (int z = 0; z < locations.length; z++) {
-                        //locations[x][y][z] =
-                    }
+    public void paint(Graphics g) {
+        Graphics bg = buffer.getGraphics();
+        bg.setColor(Color.BLACK);
+        bg.fillRect(0,0,getWidth(),getHeight());
+        bg.setColor(Color.WHITE);
+        for (int x = 0; x < locations.length; x++) {
+            for (int y = 0; y < locations.length; y++) {
+                for (int z = 0; z < locations.length; z++) {
+                    bg.drawRect(40,40,40,40);
                 }
             }
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-            bg.drawRect(10,10,40,40);
-
-
-            g.drawImage(buffer,0,0,null);
         }
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
+        bg.drawRect(10,10,40,40);
 
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
 
-            repaint();
-        }
+        g.drawImage(buffer,0,0,null);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+
+        repaint();
+    }
 
 //        public int getStatus() {
-  //          return PLAYING;
-    //    }
+//          return PLAYING;
+//    }
 
-        @Override
-        public void mousePressed(MouseEvent e) {
+    @Override
+    public void mousePressed(MouseEvent e) {
 
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-
-        public void addNotify() {
-            super.addNotify();
-            requestFocus();
-        }
-
-        public void update() {
-            //       game.update();
-        }
     }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
+    }
+}
